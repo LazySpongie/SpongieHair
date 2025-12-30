96f2e98ffc164ad572dc2cf02eee29b6e571284c
Spongie <lazyspongie@gmail.com>
2025-12-30 02:43:34 +0000

    Replaced fbx files with gltf and added Taurie

--- Contents/mods/Spongie Hair/42.0/media/lua/client/SpongieHair_GelledHair.lua
+++ Contents/mods/Spongie Hair/42.0/media/lua/client/SpongieHair_GelledHair.lua
@@ -1,13 +1,16 @@
-local activatedMods = {}
-local mods = getActivatedMods()
-for i = 0, mods:size() - 1 do
-    local shortId = string.match(mods:get(i), "\\(.+)$")
-    activatedMods[shortId] = true
-	-- local s = luautils.split(mods:get(i), "\\");
-    -- activatedMods[s[1]] = true
-end
+---@diagnostic disable: undefined-global
+-- local activatedMods = {}
+-- local mods = getActivatedMods()
+-- for i = 0, mods:size() - 1 do
+--     local shortId = string.match(mods:get(i), "\\(.+)$")
+--     activatedMods[shortId] = true
+-- 	-- local s = luautils.split(mods:get(i), "\\");
+--     -- activatedMods[s[1]] = true
+-- end
 
-if activatedMods["SpnHairAPI"] then require("SpongieHairAPI") else return end
+local API_ENABLED = getActivatedMods():contains("\\SpnHairAPI")
+if not API_ENABLED then return end
+require("SpongieHairAPI")
 
 SpongieHairAPI:RequireHairGelOrHairSpray("M_Vergil", "M_Dante")
 SpongieHairAPI:RequireHairGelOrHairSpray("F_Vergil", "F_Dante")
