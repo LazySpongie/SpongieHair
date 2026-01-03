a84d91228025a28b712d644f0b80d0fa123fc8be
Spongie <lazyspongie@gmail.com>
2026-01-03 00:11:05 +0000

    Update SpongieHair_GelledHair.lua

--- Contents/mods/Spongie Hair/42.0/media/lua/shared/SpongieHair_GelledHair.lua
+++ Contents/mods/Spongie Hair/42.0/media/lua/shared/SpongieHair_GelledHair.lua
@@ -2,27 +2,28 @@
 local API_ENABLED = getActivatedMods():contains("\\SpnHairAPI")
 if not API_ENABLED then return end
 
-print("using SpongieHairAPI")
+print("SpongieHair using SpongieHairAPI")
 
 local SpongieHairAPI = require("SpongieHairAPI")
 
 local hairs = {
 	{name = "M_Vergil", flatHair = "M_Dante", hairGel = true, hairSpray = true},
 	{name = "F_Vergil", flatHair = "F_Dante", hairGel = true, hairSpray = true},
-	{name = "M_RickShow", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "F_RickShow", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "M_SpikedUp", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "F_SpikedUp", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "M_SpikedForward", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "F_SpikedForward", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "M_SpikedMessy", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "F_SpikedMessy", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "M_AshSlick", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "F_AshSlick", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "M_Frank", flatHair = "", hairGel = true, hairSpray = false},
-	{name = "F_Frank", flatHair = "", hairGel = true, hairSpray = false},
+	{name = "M_RickShow", hairGel = true},
+	{name = "F_RickShow", hairGel = true},
+	{name = "M_SpikedUp", hairGel = true},
+	{name = "F_SpikedUp", hairGel = true},
+	{name = "M_SpikedForward", hairGel = true},
+	{name = "F_SpikedForward", hairGel = true},
+	{name = "M_SpikedMessy", hairGel = true},
+	{name = "F_SpikedMessy", hairGel = true},
+	{name = "M_AshSlick", hairGel = true},
+	{name = "F_AshSlick", hairGel = true},
+	{name = "M_Frank", hairGel = true},
+	{name = "F_Frank", hairGel = true},
 }
 
 for i,v in ipairs(hairs) do
 	SpongieHairAPI:AddHair(v)
 end
+hairs = nil
